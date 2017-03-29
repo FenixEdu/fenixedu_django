7bf1cb30d017c7aa6d45b32a6c3283b6778b962a
Paulo Branco <pauloricardocb@gmail.com>
2017-03-29 23:30:20 +0100

    Release v1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup, Extension
 
 setup(name='fenixedu_django',
-		version='1.0.1',
+		version='1.0.2',
 		description='FenixEdu Django utilities',
 		author='Samuel Coelho',
 		author_email='samuelfcmc@gmail.com',
